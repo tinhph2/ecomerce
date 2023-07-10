95ccc22c4a7052c7ed1a0cd8e7b03bb9f7ea4034
tinhph2 <tinhph2@fe.edu.vn>
2023-07-10 19:29:50 +0700

    add

--- cart/cart.py
+++ cart/cart.py
@@ -7,3 +7,14 @@ class Cart():
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
         self.cart = cart
+
+    def add(self, product, product_qty):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_qty
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
+        self.session.modified = True
+    
+    def __len__(self):
+        return sum(item['qty'] for item in self.cart.values())
--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,15 @@ def cart_summary(request):
     return render(request, 'cart/cart-summary.html')
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, product_qty=product_quantity)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
