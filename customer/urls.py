c681642b7a5b640c39a8cd170db9b9d06376a738
tinhph2 <tinhph2@fe.edu.vn>
2023-07-12 20:50:12 +0700

    add

--- customer/urls.py
+++ customer/urls.py
@@ -4,10 +4,9 @@ from customer import views
 
 urlpatterns = [
     path('add', views.add_customer, name="add_customer"),
-    path('list/', views.list_customer, name='list_customer'),
+    path('', views.list_customer, name='list_customer'),
     path('add_new_custom/', views.add_new_customer, name='add_new_customer'),
     path('update_customer/<int:customer_id>/',views.update_customer,name='update_customer'),
     path('update_process/', views.update_process, name='update_process'),
     path('delete_customer/', views.delete_customer, name='delete_customer'),
-    
 ]
