a8edc240e0ff5860cebab3c913c71cbe267fbf5d
tinhph2 <tinhph2@fe.edu.vn>
2023-07-21 20:28:25 +0700

    add send mail

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.login, name="login"),
     path('logout',views.logout, name="logout"),
     path('process_login', views.process_login, name="process_login"),
+    path('register', views.register, name="register"),
+    path('process_regis', views.process_regis, name="process_regis"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib import auth
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 def login(request):
     return render(request,"account/login.html") 
@@ -19,4 +23,24 @@ def process_login(request):
 def logout(request):
     auth.logout(request)
     return render(request,"account/login.html") 
-        
+
+def register(request):
+    return render(request,"account/register.html") 
+
+def process_regis(request):
+    if request.method == "POST":
+        username = request.POST["userName"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+        user = User.objects.create_user(
+            username = username,
+            password = password,
+            email = email
+        )
+        subject = "Welcome my shop"
+        mess = f'Hi {user.username}, thanks for regis'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [user.email,]
+        send_mail(subject, mess, email_from, recipient_list)
+        return redirect('login')
+    return render(request,"account/register.html") 
